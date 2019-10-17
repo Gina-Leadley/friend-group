208636522b69806dd35982bfe3bfd89aca1690b2
Georgina Leadley <zcemgcv@ucl.ac.uk>
2019-10-17 11:56:55 +0100

    Fix

--- group.py
+++ group.py
@@ -1,5 +1,5 @@
 """An example of how to represent a group of acquaintances in Python."""
-import pandas as import pd
+import pandas as pd
 Jill = {"Name" : "Jill", "Age" : "26", "Job" : "Biologist", "Connections" : ("Zalika's friend", "John's partner")}
 Zalika = {"Name" : "Zalika", "Age" : "28", "Job" : "Artist", "Connections" : "Jill's friend"}
 John = {"Name" : "John", "Age" : "27", "Job" : "Writer"}
